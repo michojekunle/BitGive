3e4f37769af76a63dbbf2f1fe05ffd908e91d06c
AMD <michojekunle1@gmail.com>
2025-04-25 16:44:10 +0100

    implementing the use get nfts hook

---
+++ frontend/hooks/use-get-nfts.tsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+import { getContract, readContract, resolveMethod } from "thirdweb";
+import { useActiveAccount } from "thirdweb/react";
+import { contracts } from "@/lib/contract";
+import { client, rootstockTestnet } from "@/lib/config";
+import useFetchCampaigns from "./use-fetch-campaigns";
+import { publicClient } from "@/lib/client";
+import { Abi, formatUnits } from "viem";
+
+export interface NFTMetadata {
+  id: number;
+  title: string;
+  image: string;
+  charity: string;
+  date: number;
+  tier: string;
+}
+
+const useGetNfts = () => {
+  const account = useActiveAccount();
+  const address = account?.address;
+  const { getCampaignsDetails } = useFetchCampaigns();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const contract = getContract({
+    client,
+    address: contracts.nftReward.address,
+    chain: rootstockTestnet,
+  });
+
+  const getOwnerNFTs = async (): Promise<NFTMetadata[]> => {
+    if (!address) return [];
+    try {
+      setIsLoading(true);
+      const tokenIds = (await readContract({
+        contract,
+        method: resolveMethod("getTokensByOwner"),
+        params: [address],
+      })) as any;
+
+      const rawMetadataTxs = tokenIds.map((id: any) => ({
+        address: contracts.nftReward.address,
+        abi: contracts.nftReward.abi as Abi,
+        functionName: "getNFTMetadata",
+        args: [id],
+      }));
+
+      const rawTokenUriTxs = tokenIds.map((id: any) => ({
+        address: contracts.nftReward.address,
+        abi: contracts.nftReward.abi as Abi,
+        functionName: "tokenURI",
+        args: [id],
+      }));
+
+      const rawTxs = [...rawMetadataTxs, ...rawTokenUriTxs];
+
+      const results = await publicClient.multicall({
+        contracts: rawTxs,
+      });
+
+      const midIndex = results.length / 2;
+      const nftMetadataResults = results.slice(0, midIndex);
+      const tokenUriResults = results.slice(midIndex);
+
+      const tokenUris = tokenUriResults.map(
+        ({ result }: { result?: any; status: string; error?: Error }) => result
+      );
+
+      const tokenDetails = await Promise.all(
+        tokenUris.map(async (uri) => {
+          const response = await fetch(uri);
+          return response.json();
+        })
+      );
+
+      console.log(tokenDetails);
+
+      return results
+        .filter(({ status }) => status === "success")
+        .map(
+          (
+            { result }: { result?: any; status: string; error?: Error },
+            idx: number
+          ) => ({
+            ...result,
+            id: Number(result.id),
+            campaignId: Number(result.campaignId),
+            timestamp: Number(result.timestamp) * 1000, //converting time to milliseconds
+            amount: Number(formatUnits(result.amount, 18)),
+            campaignName: campaigns[idx].name,
+          })
+        );
+    } catch (error: any) {
+      setError(
+        error.message || "An error occured while fetching Donor Donations"
+      );
+    } finally {
+      setIsLoading(false);
+    }
+
+    return [];
+  };
+
+  return { getOwnerNfts, isLoading, error };
+};
+
+export default useGetNfts;
