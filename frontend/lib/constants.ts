4ff9a928055c8704025f7e26676488b1a49efc11
A M D <90222287+michojekunle@users.noreply.github.com>
2025-04-29 11:04:42 +0100

    Update constants.ts

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -1,6 +1,6 @@
 export const NftBadgeTierUris = {
-    "Bronze": "",
-    "Silver": "",
-    "Gold": "",
+    "Bronze": "https://gateway.pinata.cloud/ipfs/bafybeid5hhgttpt72k4btwhbvnprqowuzsd5gc4iump4koekpwftrk5cba",
+    "Silver": "https://gateway.pinata.cloud/ipfs/bafybeibkdz5piy4u5txr3jbhbn5thzhhkv2ogqwbnuts2tkgqcan7uxqcm",
+    "Gold": "https://gateway.pinata.cloud/ipfs/bafybeigtx5qpoicvpzlfsqn2bj4odxqahpaj4myzywhy2l65rxbrtl3k7y",
 } as Record<string, string>;
 
